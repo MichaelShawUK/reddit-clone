133bc663bf7e0143094f55fbf11bd44811c92edb
MichaelShawUK <MichaelShawUK@gmail.com>
2023-01-27 22:14:38 +0000

    Sync comment submission with database and front end

--- src/Components/Reply.js
+++ src/Components/Reply.js
@@ -2,9 +2,11 @@ import { Form } from "react-router-dom";
 import { useContext } from "react";
 import { LoggedInContext } from "../App";
 import { getAuth } from "firebase/auth";
+import { db } from "../firebaseInit";
 import "../css/reply.css";
+import { collection, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 
-const submitComment = (e, post) => {
+async function submitComment(e, post) {
   const comment = {
     body: e.target.form[0].value,
     downvotes: 0,
@@ -17,8 +19,16 @@ const submitComment = (e, post) => {
       username: getAuth().currentUser.displayName.split(" ").join(""),
     },
   };
-  console.log(comment);
-};
+  const docRef = doc(collection(db, "comments"));
+  await setDoc(docRef, comment);
+  const docSnap = await getDoc(docRef);
+  await updateDoc(docRef, { id: docSnap.id });
+  const postRef = doc(db, "posts", comment.postId);
+  const postSnap = await getDoc(postRef);
+  const origCommentIds = postSnap.data().commentIds;
+  const updatedCommentIds = [...origCommentIds, docSnap.id];
+  await updateDoc(postRef, { commentIds: updatedCommentIds });
+}
 
 const Reply = ({ post }) => {
   const { loggedIn, setLoggedIn } = useContext(LoggedInContext);
