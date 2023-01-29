303895d17f8d80e262be5474127efe6b135cafd9
MichaelShawUK <MichaelShawUK@gmail.com>
2023-01-29 12:40:29 +0000

    Disable upvote and downvotes if not logged in

--- src/Components/Votes.js
+++ src/Components/Votes.js
@@ -1,11 +1,23 @@
+import { useContext } from "react";
+import { LoggedInContext } from "../App";
 import "../css/votes.css";
 
 const Votes = ({ post, voteClick }) => {
+  const { loggedIn, setLoggedIn } = useContext(LoggedInContext);
+
   return (
     <div className="votes">
-      <button className="upvote vote" onClick={voteClick}></button>
+      <button
+        className="upvote vote"
+        disabled={!loggedIn}
+        onClick={voteClick}
+      ></button>
       <div className="vote-num">{post.upvotes - post.downvotes}</div>
-      <button className="downvote vote" onClick={voteClick}></button>
+      <button
+        className="downvote vote"
+        disabled={!loggedIn}
+        onClick={voteClick}
+      ></button>
     </div>
   );
 };
