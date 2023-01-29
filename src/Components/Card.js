ae4fcad174a7504100f795c161ee1db31c040028
MichaelShawUK <MichaelShawUK@gmail.com>
2023-01-29 12:58:51 +0000

    Allow logged in users to upvote or downvote posts

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,11 +3,30 @@ import Votes from "./Votes";
 import CardHeader from "./CardHeader";
 import PostPreview from "./PostPreview";
 import CardFooter from "./CardFooter";
+import { useContext } from "react";
+import { LoggedInContext } from "../App";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebaseInit";
 
 const Card = ({ post }) => {
+  const { loggedIn, setLoggedIn } = useContext(LoggedInContext);
+
+  async function voteClick(e) {
+    if (loggedIn) {
+      const postRef = doc(db, "posts", post.id);
+      const classList = Array.from(e.target.classList);
+      if (classList.includes("upvote")) {
+        await updateDoc(postRef, { upvotes: post.upvotes + 1 });
+      } else if (classList.includes("downvote")) {
+        await updateDoc(postRef, { downvotes: post.downvotes + 1 });
+      }
+      window.location.reload();
+    }
+  }
+
   return (
     <div className="card">
-      <Votes post={post} />
+      <Votes post={post} voteClick={voteClick} />
       <CardHeader post={post} />
       <div className="post-title">{post.title}</div>
       <PostPreview post={post} />
